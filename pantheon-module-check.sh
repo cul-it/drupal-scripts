eb9c2e7b970fe7622f4b50b76962ce55aed6c09e
James Reidy <jreidy@cornell.edu>
2019-05-31 10:55:17 -0400

    be sure the alias points to a drupal site

--- pantheon-module-check.sh
+++ pantheon-module-check.sh
@@ -14,6 +14,8 @@ SEEKING=$1
 for alias in $(drush sa)
 do
   if [[ $alias == *".live" ]]; then
+    DRUPAL=`drush $alias variable-get maintenance_mode_message`
+    ERR=$?; if [[ $ERR != 0 ]]; then continue; fi
     NAME=`echo $alias  | cut -d. -f 2-3`
     LIST=`drush "$alias" pml --status=enabled --no-core --type=module --format=list`
     OUTPUT="$NAME"
